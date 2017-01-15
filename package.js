679187796c0a7fe182b966ebafa55474e1eeef05
Abigail Watson <awatson1978@gmail.com>
2017-01-15 16:04:48 -0600

    FHIR Conditions CRUD and 66 validation tests

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-condition',
-  version: '1.1.3',
+  version: '1.1.7',
   summary: 'HL7 FHIR Resource - Condition',
   git: 'https://github.com/clinical-meteor/hl7-resource-condition',
   documentation: 'README.md'
@@ -17,7 +17,7 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.6.0');
+  api.use('clinical:hl7-resource-datatypes@0.6.9');
 
   api.addFiles('lib/hl7-resource-condition.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
--- server/initialize.js
+++ server/initialize.js
@@ -5,6 +5,84 @@ Meteor.startup(function () {
     if (Conditions.find().count() === 0) {
     console.log('No Conditions found.  Creating some...');
 
+      var fever = {
+        "resourceType": "Condition",
+        "patient": {
+          "reference": "Patient/f201",
+          "display": "Jane Doe"
+        },
+        "asserter": {
+          "reference": "Practitioner/f201"
+        },
+        "dateRecorded": "2013-04-04",
+        "code": {
+          "coding": [
+            {
+              "system": "http://snomed.info/sct",
+              "code": "386661006",
+              "display": "Fever"
+            }
+          ]
+        },
+        "category": {
+          "coding": [
+            {
+              "system": "http://snomed.info/sct",
+              "code": "55607006",
+              "display": "Problem"
+            },
+            {
+              "system": "http://hl7.org/fhir/condition-category",
+              "code": "finding"
+            }
+          ]
+        },
+        "clinicalStatus": "active",
+        "verificationStatus": "confirmed",
+        "severity": {
+          "coding": [
+            {
+              "system": "http://snomed.info/sct",
+              "code": "255604002",
+              "display": "Mild"
+            }
+          ]
+        },
+        "onsetDateTime": "2013-04-02",
+        "evidence": [
+          {
+            "code": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "258710007",
+                  "display": "degrees C"
+                }
+              ]
+            },
+            "detail": [
+              {
+                "reference": "Observation/f202",
+                "display": "Temperature"
+              }
+            ]
+          }
+        ],
+        "bodySite": [
+          {
+            "coding": [
+              {
+                "system": "http://snomed.info/sct",
+                "code": "38266002",
+                "display": "Entire body as a whole"
+              }
+            ]
+          }
+        ]
+      };
+
+    Conditions.insert(fever);
+
 
 
     }
