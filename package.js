7543e67a40df8a9d89cf2d7f4125ceebb3bbce33
Abigail Watson <awatson1978@gmail.com>
2017-03-31 18:16:40 -0500

    DTSU3 datatypes; fhirVersion

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-condition',
-  version: '1.1.7',
+  version: '1.1.10',
   summary: 'HL7 FHIR Resource - Condition',
   git: 'https://github.com/clinical-meteor/hl7-resource-condition',
   documentation: 'README.md'
@@ -14,10 +14,10 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('prime8consulting:meteor-oauth2-server@0.0.2');
+  api.use('clinical:fhir-vault-server@0.0.3');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.6.9');
+  api.use('clinical:hl7-resource-datatypes@3.0.0');
 
   api.addFiles('lib/hl7-resource-condition.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
