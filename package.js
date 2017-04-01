014ea37c97659468adf9ef3fa007200cfb037ac7
Abigail Watson <awatson1978@gmail.com>
2017-03-31 23:44:35 -0500

    initial REST API

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-condition',
-  version: '1.1.10',
+  version: '1.2.0',
   summary: 'HL7 FHIR Resource - Condition',
   git: 'https://github.com/clinical-meteor/hl7-resource-condition',
   documentation: 'README.md'
