391724cb6032447aa1a286e7c4fc9bd70d84531d
Abigail Watson <awatson1978@gmail.com>
2020-02-06 14:56:00 -0600

    CardiacAccreditationUtility@0.0.7

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-condition',
-  version: '1.9.2',
+  version: '1.9.3',
   summary: 'HL7 FHIR Resource - Condition',
   git: 'https://github.com/clinical-meteor/hl7-resource-condition',
   documentation: 'README.md'
